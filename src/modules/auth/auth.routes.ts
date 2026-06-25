a99ab974545e7d7a2e75961a59bd86a20a0ad5ff
Araby Hossain Abid <arabyhossainabid@gmail.com>
2026-06-25 16:35:00 +0600

    feat: setup authentication endpoints

---
+++ src/modules/auth/auth.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "../../lib/auth.js";
+
+const router = Router();
+
+router.all("/*", toNodeHandler(auth));
+
+export default router;
